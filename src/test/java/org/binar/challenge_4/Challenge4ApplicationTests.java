61fa7e745addaad9eed77e0ef9b25aa1bb38b1a0
haris <haris.aulia@gmail.com>
2022-09-11 14:07:59 +0700

    initial commit

---
+++ src/test/java/org/binar/challenge_4/Challenge4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binar.challenge_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
