8a004ae76b44cd11e361ed23c10550ede1dfdb9c
haris <haris.aulia@gmail.com>
2022-09-17 21:57:17 +0700

    add enableJpaAuditing di main app

--- src/main/java/org/binar/challenge_4/Challenge4Application.java
+++ src/main/java/org/binar/challenge_4/Challenge4Application.java
@@ -2,8 +2,11 @@ package org.binar.challenge_4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Challenge4Application {
 
 	public static void main(String[] args) {
