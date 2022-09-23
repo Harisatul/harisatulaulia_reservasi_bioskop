e5b543f76a72f66088aefd80d5abee7eb29e73a5
haris <haris.aulia@gmail.com>
2022-09-23 14:46:46 +0700

    added MovieRepository repository

---
+++ src/main/java/org/binar/challenge_4/repository/MovieRepository.java
@@ -0,0 +1,14 @@
+package org.binar.challenge_4.repository;
+
+import org.binar.challenge_4.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Override
+    Optional<Movie> findById(Long aLong);
+}
