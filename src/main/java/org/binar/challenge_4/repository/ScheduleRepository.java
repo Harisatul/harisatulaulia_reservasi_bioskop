12ab64d359a5b3e676e261af123b5ea8428d7094
haris <haris.aulia@gmail.com>
2022-09-23 15:43:02 +0700

    added ScheduleRepository

---
+++ src/main/java/org/binar/challenge_4/repository/ScheduleRepository.java
@@ -0,0 +1,10 @@
+package org.binar.challenge_4.repository;
+
+import org.binar.challenge_4.entities.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+}
