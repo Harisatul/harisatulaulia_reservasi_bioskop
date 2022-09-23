399186cd33db299255c7add715dc81516507b033
haris <haris.aulia@gmail.com>
2022-09-23 14:41:44 +0700

    added JsonIgnored annotation to fixing bug

--- src/main/java/org/binar/challenge_4/entities/Movie.java
+++ src/main/java/org/binar/challenge_4/entities/Movie.java
@@ -1,10 +1,10 @@
 package org.binar.challenge_4.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -16,9 +16,11 @@ public class Movie extends BaseEntity{
     private String description;
     private Boolean isPremiered;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "movies_genre")
     private Set<Genre> movies ;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "movies")
     private Set<Schedule> schedules;
 
