33aad1f0a66eeaa3438e29c6655c8ed2aea24a82
haris <haris.aulia@gmail.com>
2022-09-23 15:04:44 +0700

    added MovieService interface

---
+++ src/main/java/org/binar/challenge_4/service/MovieService.java
@@ -0,0 +1,12 @@
+package org.binar.challenge_4.service;
+
+import org.binar.challenge_4.entities.Movie;
+import org.binar.challenge_4.payload.ApiResponse;
+
+public interface MovieService {
+
+    ApiResponse getAllMovie();
+    ApiResponse addMovie(Movie movie);
+    ApiResponse updateMovie(Long id);
+
+}
