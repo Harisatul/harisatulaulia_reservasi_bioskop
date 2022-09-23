fc9fa367aebb92f0bcad0c07b033308b3a90a3d4
haris <haris.aulia@gmail.com>
2022-09-23 14:42:36 +0700

    added few method to automatically created timestamp when object persisting or updating

--- src/main/java/org/binar/challenge_4/entities/BaseEntity.java
+++ src/main/java/org/binar/challenge_4/entities/BaseEntity.java
@@ -31,4 +31,23 @@ public class BaseEntity implements Serializable {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @PrePersist
+    protected void prePersist() {
+        if (this.createdAt == null) createdAt = LocalDateTime.now();
+        if (this.updatedAt == null) updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreRemove
+    protected void preRemove() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+
+
 }
