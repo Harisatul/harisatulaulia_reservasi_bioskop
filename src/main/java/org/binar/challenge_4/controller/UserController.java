c8adca515bba976a2630a554e53043e519d36a89
haris <haris.aulia@gmail.com>
2022-09-23 09:02:40 +0700

    added deleteUser controller

--- src/main/java/org/binar/challenge_4/controller/UserController.java
+++ src/main/java/org/binar/challenge_4/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.binar.challenge_4.controller;
 
 import org.binar.challenge_4.entities.Users;
+import org.binar.challenge_4.payload.ApiResponse;
 import org.binar.challenge_4.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +35,12 @@ public class UserController {
     public ResponseEntity<Users> updateUser(@RequestBody Users user, @PathVariable(value = "username") String username){
         Users users = userService.updateUser(user, username);
         return new ResponseEntity<>(users, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{username}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable(value = "username") String username){
+        ApiResponse apiResponse = userService.deleteUsers(username);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
 
