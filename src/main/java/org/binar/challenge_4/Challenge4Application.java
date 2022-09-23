14b96c7250ed455638d9162b34d9767dc0e36bfe
haris <haris.aulia@gmail.com>
2022-09-23 22:41:27 +0700

    finished all requirement features

--- src/main/java/org/binar/challenge_4/Challenge4Application.java
+++ src/main/java/org/binar/challenge_4/Challenge4Application.java
@@ -2,11 +2,15 @@ package org.binar.challenge_4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "org.binar.challenge_4.repository")
 public class Challenge4Application {
 
 	public static void main(String[] args) {
