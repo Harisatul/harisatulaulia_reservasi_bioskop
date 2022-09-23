c3f228c10f2536ec24dbe4bc036b440048834606
haris <haris.aulia@gmail.com>
2022-09-23 15:37:25 +0700

    added scheduled service implement

---
+++ src/main/java/org/binar/challenge_4/service/ScheduleServiceImpl.java
@@ -0,0 +1,15 @@
+package org.binar.challenge_4.service;
+
+import org.binar.challenge_4.payload.ApiResponse;
+
+public class ScheduleServiceImpl implements ScheduleService {
+    @Override
+    public ApiResponse getAllSchedule() {
+        return null;
+    }
+
+    @Override
+    public ApiResponse getScheduleFromMovie() {
+        return null;
+    }
+}
