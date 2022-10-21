38a54f133e136e5a4ec30c672b3da245179d2b79
haris <haris.aulia@gmail.com>
2022-10-21 18:46:27 +0700

    defined findUserByEmail method

--- src/main/java/org/binar/challenge_4/repository/UserRepository.java
+++ src/main/java/org/binar/challenge_4/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<Users, Long> {
 
    Optional<Users> findUsersByUsername(String name);
+   Optional<Users> findUsersByEmail(String email);
 }
