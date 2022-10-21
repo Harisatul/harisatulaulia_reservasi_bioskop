6bf211d3899e0cf44b67fc86a844f7750c1b6596
haris <haris.aulia@gmail.com>
2022-10-21 18:47:03 +0700

    changed addUser method parameter

--- src/main/java/org/binar/challenge_4/service/UserService.java
+++ src/main/java/org/binar/challenge_4/service/UserService.java
@@ -1,5 +1,6 @@
 package org.binar.challenge_4.service;
 
+import org.binar.challenge_4.dto.UserDTO;
 import org.binar.challenge_4.entities.Users;
 import org.binar.challenge_4.payload.ApiResponse;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,7 @@ import java.util.List;
 public interface UserService {
 
     List<Users> getAllUsers();
-    ResponseEntity<Users> addUser(Users user);
+    ResponseEntity<Users> addUser(UserDTO users);
     Users updateUser(Users newUser, String name);
     Users getUserByUsername(String username);
     ApiResponse deleteUsers(String username);
