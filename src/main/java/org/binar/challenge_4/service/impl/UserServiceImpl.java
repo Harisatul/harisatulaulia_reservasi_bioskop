7851c338063b983557389b74ec85633f2705cd36
haris <haris.aulia@gmail.com>
2022-10-21 14:35:51 +0700

    added implement of getUserByUsername method

--- src/main/java/org/binar/challenge_4/service/impl/UserServiceImpl.java
+++ src/main/java/org/binar/challenge_4/service/impl/UserServiceImpl.java
@@ -9,18 +9,39 @@ import org.binar.challenge_4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users users = userRepository.findUsersByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found in database "));
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        users.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(users.getUsername(), users.getPassword(), authorities);
+    }
+
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -34,6 +55,7 @@ public class UserServiceImpl implements UserService {
             ApiResponse apiResponse = new ApiResponse(Boolean.FALSE, "Username is already taken");
             throw new BadRequestException(apiResponse);
         }
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         Users save = userRepository.save(user);
         return new ResponseEntity<>(save, HttpStatus.CREATED);
     }
@@ -47,6 +69,14 @@ public class UserServiceImpl implements UserService {
             users.setPassword(newUser.getPassword());
             userRepository.save(users);
         }
+        return users;
+    }
+
+    @Override
+    public Users getUserByUsername(String username) {
+        System.out.println(username);
+        Users users = userRepository.findUsersByUsername(username).orElseThrow(
+                () -> new ResourceNotFoundException("User", "username", username));
         return users;
     }
 
